8b290b4f9e2e872d28b54c1952e97626897215c0
cherryandlove <j.lewicka@rocketmail.com>
2017-09-07 21:52:50 +0100

    upload final version

---
+++ js/script.js
@@ -0,0 +1,217 @@
+// QUOTES DATA
+
+var quotes = [
+	{
+	quote: 'The tragedy in life doesn\'t lie in not reaching your goal. The tragedy lies in having no goal to reach.',
+	author: 'Benjamin Mays'
+	},
+	{
+	quote: 'By recording your dreams and goals on paper, you set in motion the process of becoming the person you most want to be. Put your future in good hands - your own.',
+	author: 'Mark Victor Hansen'
+	}, 
+	{
+	quote: 'Give yourself an even greater challenge than the one you are trying to master and you will develop the powers necessary to overcome the original difficulty.',
+	author: 'William J. Bennett \'The Book of Virtues\''
+	},
+	{
+	quote: 'The entrepreneur is essentially a visualizer and actualizer. He can visualize something, and when he visualizes it he sees exactly how to make it happen.',
+	author: 'Robert L. Schwartz'
+	},
+	{
+	quote: 'If you are going to achieve excellence in big things, you develop the habit in little matters. Excellence is not an exception, it is a prevailing attitude.',
+	author: 'Charles R. Swindoll'
+	},
+	{
+	quote: 'Desire is the key to motivation, but it\'s determination and commitment (...) a commitment to excellence - that will enable you to attain the success you seek.',
+	author: 'Mario Andretti'
+	},
+	{
+	quote: 'The secret of joy in work is contained in one word - excellence. To know how to do something well is to enjoy it.',
+	author: 'Pearl Buck'
+	},
+	{
+	quote: 'The quality of a person\'s life is in direct proportion to their commitment to excellence, regardless of their chosen field of endeavor.',
+	author: 'Vince Lombardi'
+	},
+	{
+	quote: 'Enter every activity without giving mental recognition to the possibility of defeat. Concentrate on your strengths, instead of your weaknesses\' on your powers, instead of your problems.',
+	author: 'Paul J. Meyer'
+	},
+	{
+	quote: 'Knowing trees, I understand the meaning of patience. Knowing grass, I can appreciate persistence.', 
+	author: 'Hal Borland'
+	},
+	{
+	quote: 'An invincible determination can accomplish almost anything and in this lies the great distinction between great men and little men',
+	author: 'Thomas Fuller'
+	},
+	{
+	quote: 'The task of leadership is not to put greatness into people, but to elicit it, for the greatness is there already.',
+	author: 'John Buchan'
+	},
+	{
+	quote: 'Be patient with yourself. Self-growth is tender; it\'s holy ground. There\'s no greater investment.',
+	author: 'Stephen Covey'
+	},
+	{
+	quote: 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.', 
+	author: 'Norman Vincent Peale'
+	},
+	{
+	quote: 'Success consists of going from failure to failure without loss of enthusiasm.', 
+	author: 'Winston Churchill'
+	},
+	{
+	quote: 'Successful and unsuccessful people do not vary greatly in their abilities. They vary in their desires to reach their potential.',
+	author: 'John Maxwell'
+	},
+	{
+	quote: 'Would you like me to give you a formula for success? (...) go ahead and make mistakes. Make all you can. Because remember that\'s where you will find success.',
+	author: 'Thomas J. Watson'
+	},
+	{
+	quote: 'It is a paradoxical but (...) the most likely way to reach a goal is to be aiming not at that goal itself but at some more ambitious goal beyond it.', 
+	author: 'Arnold Toynbee'
+	},
+	{
+	quote: 'My attitude is that if you push me towards something that you think is a weakness, then I will turn that perceived weakness into a strength.', 
+	author: 'Michael Jordan'
+	},
+	{
+	quote: 'Pain is temporary. It may last a minute, or an hour, or a day, or a year, but eventually (...) something else will take its place. If I quit, however, it lasts forever.',
+	author: 'Lance Armstrong'
+	},
+	{ 
+	quote: 'Strength does not come from winning. Your struggles develop your strengths. When you go through hardships and decide not to surrender, that is strength.',
+	author: 'Arnold Schwarzenegger'
+	},
+	{
+	quote: 'I know what I have to do, and I\'m going to do whatever it takes. If I do it, I\'ll come out a winner, and it doesn\'t matter what anyone else does.',
+	author: 'Florence Griffith Joyner'
+	},
+	{
+	quote: 'Every time you stay out late, (...) you sleep in, (...) you miss a workout, (...) you don\'t give 100% \'. You make it that much easier for me to beat you\'.',
+	author: 'Unknown'
+	},
+	{
+	quote: 'If you can imagine it, you can achieve it. If you can dream it, you can become it.',
+	author: 'William Arthur Ward'
+	},
+	{
+	quote: 'It is the mark of an educated mind to be able to entertain a thought without accepting it.',
+	author: 'Aristotle'
+	},
+	{
+	quote: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
+	author: 'Albert Schweitzer'
+	},
+	{
+	quote: 'Wear your learning, like your watch, in a private pocket: and do not pull it out and strike it, merely to show that you have one.',
+	author: 'Philip Stanhope, 4th Earl of Chesterfield'
+	},
+	{
+	quote: 'He who asks is a fool for five minutes, but he who does not ask remains a fool forever.',
+	author: 'Chinese Proverb'
+	},
+	{
+	quote: 'Study while others are sleeping; work while others are loafing, prepare while others are playing; and dream while others are wishing.',
+	author: 'William Arthur Ward'
+	},
+	{
+	quote: 'Books can be dangerous. The best ones should be labeled \'This could change your life\'.',
+	author: 'Helen Exley'
+	},
+	{
+	quote: 'A book is like a garden carried in the pocket.',
+	author: 'Chinese Proverb'
+	},
+	{
+	quote: 'Change your thoughts and you change your world.',
+	author: 'Norman Vincent Peale'
+	},
+	{
+	quote: 'Your time is limited, don\'t waste it living someone else\'s life (...) have the courage to follow your heart and intuition (...).',
+	},
+	{
+	quote: 'Nobody can go back and start a new beginning, but anyone can start today and make a new ending.',
+	author: 'Maria Robinson'
+	},
+	{
+	quote: 'Out of clutter, find Simplicity. From discord, find Harmony. In the middle of difficulty lies Opportunity.',
+	author: 'Albert Einstein'
+	},
+	{
+	quote: 'Yesterday is not ours to recover, but tomorrow is ours to win or lose.',
+	author: 'Lyndon Johnson'
+	},
+	{
+	quote: 'If we did all the things we are capable of, we would astound ourselves.',
+	author: 'Thomas Edison'
+	},
+	{
+	quote: 'You miss 100% of the shots you don\'t take.',
+	author: 'Wayne Gretzky'
+	},
+	{
+	quote: 'Success is not final, failure is not fatal. It is the courage to continue that counts.',
+	author: 'Winston Churchill'
+	},
+	{
+	quote: 'What lies behind us and what lies before us are tiny matters compared to what lies within us.',
+	author: 'Ralph Waldo Emerson'
+	},
+	{
+	quote: 'Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.',
+	author: 'Mark Twain'
+	},
+	{
+	quote: 'Get going. Move forward. Aim High. Plan a takeoff. Don\'t just sit on the runway and hope someone will come along and push the airplane.',
+	author: 'Donald Trump'
+	},
+	{
+	quote: 'I decided, very early on, just to accept life unconditionally; I never expected it to do anything special for me (...) it just happened to me without my ever seeking it.',
+	author: 'Audrey Hepburn'
+	},
+	{
+	quote: 'Knowledge has to be improved, challenged, and increased constantly, or it vanishes.',
+	author: 'Peter Drucker'
+	},
+	{
+	quote: 'I\'d rather be a failure at something I love than a success at something I hate.',
+	author: 'George Burns'
+	},
+	{
+	quote: 'Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work.',
+	author: 'Stephen King'
+	},
+	{
+	quote: 'Never give up, for that is just the place and time that the tide will turn.',
+	author: 'Harriet Beecher Stowe'
+	},
+	{
+	quote: 'It\'s not what we do once in a while that shapes our lives, but what we do consistently.',
+	author: 'Tony Robbins'
+	}
+]
+
+
+
+var quoteParagraph = document.getElementById('quote');
+var quoteAuthor = document.getElementById('author');
+var twitterButton= document.getElementById('twitterButton');
+var quotesButton= document.getElementById('quotesButton');
+
+// GET THE QUOTE FUNCTION
+function uploadAQuote() {
+	var num = Math.floor(Math.random() * quotes.length);
+	quoteParagraph.innerHTML= '\"' + quotes[num].quote+ '\"';
+	quoteAuthor.innerHTML= '- ' + quotes[num].author;
+	twitterButton.setAttribute('href','https://twitter.com/intent/tweet?text=' + encodeURIComponent(quoteParagraph.innerHTML) + encodeURIComponent(quoteAuthor.innerHTML));
+};
+
+// QUOTES BUTTON
+quotesButton.addEventListener('click', function() {
+	uploadAQuote();
+	quoteParagraph.style.textAlign="justify";
+	quoteAuthor.style.textAlign="right";
+});
